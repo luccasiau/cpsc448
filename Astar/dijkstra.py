fe4edca4dacb2bf1c17101bfdaa3506dd720df27
Lucca Siaudzionis <luccasiau@gmail.com>
2020-01-19 17:24:42 -0800

    Finishing reverse-directions

--- Astar/dijkstra.py
+++ Astar/dijkstra.py
@@ -1,10 +1,6 @@
 from heapq import *
 
-def dijkstra(graph, source):
-    # Memoizing dijkstra in my graph
-    if source in graph.dist:
-        return
-
+def dijkstra(graph, source, rev=False):
     dist = {source: 0}
     q = [(0, source)]
     while q:
@@ -12,10 +8,13 @@ def dijkstra(graph, source):
         if dist[cur] < cost:
             continue
 
-        for (v, w) in graph.get_neighbours(cur):
+        for (v, w) in graph.get_neighbours(cur, rev):
             if (v not in dist) or (dist[v] > w + dist[cur]):
                 dist[v] = w + dist[cur]
                 heappush(q, (dist[v], v))
 
-    graph.dist[source] = dist
+    for (v, d) in dist.items():
+        if rev: graph.set_dist(v, source, dist[v])
+        else: graph.set_dist(source, v, dist[v])
+        
     return dist
--- Astar/graph.py
+++ Astar/graph.py
@@ -31,23 +31,29 @@ class Graph:
         self.N = max(self.N, u, v)
 
         if u not in self.edge:
-            self.edge[u] = []
-        self.edge[u].append((v, w))
+            self.edge[u] = {}
+        self.edge[u][v] = w
 
-        if n not in self.rev_edge:
-            self.rev_edge[v] = []
-        self.rev_edge[v].append((u, w))
+        if v not in self.rev_edge:
+            self.rev_edge[v] = {}
+        self.rev_edge[v][u] = w
+
+    def get_edge(self, u, v, rev=False, pi=None):
+        ref = self.edge
+        if rev is True: rev = self.rev_edge
+
+        if u not in ref: return float('inf')
+        if v not in ref[u]: return float('inf')
+        if pi is None: return ref[u][v]
+        return ref[u][v] - pi[u] + pi[v]
 
     # Returns a list of pairs (v, w) representing an edge (u, v) with cost w
     def get_neighbours(self, u, rev=False):
-        if not rev:
-            if u not in self.edge:
-                return []
-            return self.edge[u]
-        else:
-            if u not in self.rev_edge
-                return []
-            return self.rev_edge[u]
+        ref = self.edge
+        if rev is True: ref = self.rev_edge
+        
+        if u not in ref: return []
+        return ref[u].items()
 
     # If there is a shortest distance memoized, it will return it. Otherwise,
     # it will return inf.
@@ -55,3 +61,9 @@ class Graph:
         if u not in self.dist: return float('inf')
         if v not in self.dist[u]: return float('inf')
         return self.dist[u][v]
+
+    # Sets directed distance from u to v as d
+    def set_dist(self, u, v, d):
+        if u not in self.dist:
+            self.dist[u] = {}
+        self.dist[u][v] = d
